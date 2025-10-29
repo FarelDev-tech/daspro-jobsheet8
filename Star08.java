ad9ce1a59e0902dfb5def5e0b993f9fd8e34d997
fareldev <ffarelscho@gmail.com>
2025-10-29 08:58:48 +0700

    Add Percobaan1 Star08

---
+++ Star08.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Star08 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        System.out.print("Masukkan nilai n = ");
+        n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
+    }
+}
